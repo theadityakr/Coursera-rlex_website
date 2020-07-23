c53147ca141d43d33de8bb0f3cb3e4a24c2dd4d3
theadityakr <59906496+theadityakr@users.noreply.github.com>
2020-07-23 06:40:22 +0530

    footer

--- website/js/script.js
+++ website/js/script.js
@@ -17,3 +17,7 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 } 
+
+function myFunction(x) {
+  x.classList.toggle("change");
+} 
